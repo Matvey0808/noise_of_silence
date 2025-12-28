6d0432700c9dba69d93858a7104a66755aa7f84c
Chpek <avto.mat.2000g@gmail.com>
2025-12-28 19:32:30 +0700

    added a jump
    Checking ground

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -11,23 +11,43 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rb;
     private Vector2 _moveInput;
     public Transform playerBody;
+    private bool _isGround = true;
 
     private void Awake() 
     {
         _rb = GetComponent<Rigidbody>();
     }
 
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGround = false;
+        }
+    }
+
     private void Update()
     {
         _moveInput = new Vector3(
-            (Keyboard.current.wKey.isPressed) ? 1 : (Keyboard.current.sKey.isPressed) ? -1 : 0,
-            (Keyboard.current.aKey.isPressed) ? 1 : (Keyboard.current.dKey.isPressed) ? -1 : 0
+            (Keyboard.current.dKey.isPressed) ? 1 : (Keyboard.current.aKey.isPressed) ? -1 : 0,
+            (Keyboard.current.wKey.isPressed) ? 1 : (Keyboard.current.sKey.isPressed) ? -1 : 0
         ).normalized;
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && _isGround)
+        {
+            _rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector3 move = playerBody.transform.forward * _moveInput + playerBody.transform.right * _moveInput;
+        Vector3 move = playerBody.transform.forward * _moveInput.y + playerBody.transform.right * _moveInput.x;
         _rb.linearVelocity = new Vector3(move.x * speed, _rb.linearVelocity.y, move.z * speed);
     }
 }
