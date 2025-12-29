041e20f5a9925ac79460101abad56d91e668cff2
Chpek <avto.mat.2000g@gmail.com>
2025-12-29 20:29:25 +0700

    Active and None active button

--- Assets/scripts/MenuAnimation.cs
+++ Assets/scripts/MenuAnimation.cs
@@ -1,76 +1,37 @@
+using System.IO.Pipes;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MenuAnimation : MonoBehaviour
 {
-     private bool isPlaying = false;
-    private float animationProgress = 0f;
-    private Vector3 startPosition;
-    private Quaternion startRotation;
-    
-    [Header("Настройки анимации")]
-    public float duration = 1f; // Длительность анимации в секундах
-    public Vector3 moveOffset = new Vector3(0, 2, 0); // На сколько переместится
-    public Vector3 rotationAmount = new Vector3(0, 360, 0); // На сколько повернется
-    
-    void Start()
+    [SerializeField] private Button firstButton;
+    [SerializeField] private Button secondButton;
+    private Animator _anim;
+    public bool isMenu = false;
+
+    void Awake()
     {
-        startPosition = transform.position;
-        startRotation = transform.rotation;
+        _anim = GetComponent<Animator>();
     }
-    
+
     void Update()
     {
-        // Запускаем анимацию по нажатию
-        if (Keyboard.current.enterKey.wasPressedThisFrame && !isPlaying)
+        if (isMenu == true)
         {
-            StartAnimation();
+            firstButton.gameObject.SetActive(!firstButton);
+            secondButton.gameObject.SetActive(secondButton);
         }
-        
-        // Если анимация играет - обновляем
-        if (isPlaying)
+        else if (isMenu == false)
         {
-            UpdateAnimation();
-        }
-    }
-    
-    void StartAnimation()
-    {
-        isPlaying = true;
-        animationProgress = 0f;
-        Debug.Log("Анимация началась!");
-    }
-    
-    void UpdateAnimation()
-    {
-        // Увеличиваем прогресс анимации
-        animationProgress += Time.deltaTime / duration;
-        
-        // Ограничиваем прогресс от 0 до 1
-        float t = Mathf.Clamp01(animationProgress);
-        
-        // Плавная кривая (ease-in-out)
-        float smoothedT = Mathf.SmoothStep(0f, 1f, t);
-        
-        // Применяем анимацию перемещения
-        transform.position = startPosition + moveOffset * smoothedT;
-        
-        // Применяем анимацию вращения
-        transform.rotation = startRotation * Quaternion.Euler(rotationAmount * smoothedT);
-        
-        // Если анимация закончилась
-        if (t >= 1f)
-        {
-            EndAnimation();
+            firstButton.gameObject.SetActive(firstButton);
+            secondButton.gameObject.SetActive(!secondButton);
         }
     }
-    
-    void EndAnimation()
+
+    public void MenuOpen()
     {
-        isPlaying = false;
-        // Опционально: возвращаем в исходное состояние
-        // transform.position = startPosition;
-        // transform.rotation = startRotation;
-        Debug.Log("Анимация завершена!");
+        isMenu = !isMenu;
+        _anim.SetBool("isMenu", isMenu);
     }
 }
