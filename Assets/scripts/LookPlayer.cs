2c4803a6d9dbce2e37dadebe6b84dd38d35b5db2
Chpek <avto.mat.2000g@gmail.com>
2025-12-30 15:39:33 +0700

    script CameraLook

---
+++ Assets/scripts/LookPlayer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LookPlayer : MonoBehaviour
+{
+    [Header("LookPlayer")]
+    [SerializeField] private float mouseSens = 50f;
+    [SerializeField] private Transform playerBody;
+    private float _xRotate = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Mouse.current == null)
+        {
+            return;
+        }
+
+        float mouseX = Mouse.current.delta.value.x * mouseSens * Time.deltaTime;
+        float mouseY = Mouse.current.delta.value.y * mouseSens * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        _xRotate -= mouseY;
+
+        _xRotate = Mathf.Clamp(_xRotate, -90f, 90);
+        transform.localRotation = Quaternion.Euler(_xRotate, 0, 0);
+    }
+}
